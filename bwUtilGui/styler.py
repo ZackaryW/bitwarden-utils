5a3433701e50e0988559f8fc56cbcf0d6a12531f
ZackaryW <zackaryxcwang@gmail.com>
2022-07-12 13:49:37 -0700

    feat: styler add specifier

--- bwUtilGui/styler.py
+++ bwUtilGui/styler.py
@@ -1,8 +1,9 @@
 from PySide6.QtWidgets import QMainWindow, QToolBar, QStatusBar, QVBoxLayout, QPushButton, QWidget
 from PySide6.QtGui import QAction, QIcon, Qt, QFont
 
-def set_button_complete(button : QPushButton):
-    button.setEnabled(False)
+def set_button_complete(button : QPushButton, disable : bool = True):
+    if disable:
+        button.setEnabled(False)
     button.setStyleSheet("border:5px solid #00ff00; color: #FFFFFF;")
 
 def set_button_incomplete(button : QPushButton):
