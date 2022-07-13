4ccd7f4a45966498369a4e008249e4538270c7d2
ZackaryW <zackaryxcwang@gmail.com>
2022-07-12 23:37:18 -0700

    env: change startup

--- bwUtilGui/__init__.py
+++ bwUtilGui/__init__.py
@@ -1,4 +1,11 @@
-
-
 if __name__ == '__main__':
+    import logging
+    import sys
+    import os
+
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+    # disable urlib3 logging
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+    parent_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sys.path.append(parent_path)
     import bwUtilGui.__main__
--- bwUtilGui/__main__.py
+++ bwUtilGui/__main__.py
@@ -1,34 +1,27 @@
-if __name__ == "__main__":
-    import logging
-    import sys
-    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
-    # disable urlib3 logging
-    logging.getLogger("urllib3").setLevel(logging.WARNING)
-    import os
-    parent_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+import sys
+import os
+parent_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(parent_path)
 
-    sys.path.append(parent_path)
+from PySide6 import QtWidgets
+# from PySide2 import QtWidgets
+# from PyQt5 import QtWidgets
+from qt_material import apply_stylesheet
+from bwUtil.caller import BwClient
 
-    from PySide6 import QtWidgets
-    # from PySide2 import QtWidgets
-    # from PyQt5 import QtWidgets
-    from qt_material import apply_stylesheet
-    import sys
-    from bwUtil.caller import BwClient
+from bwUtilGui.mainPanel import MainPanel
+# create the application and the main window
+app = QtWidgets.QApplication(sys.argv)
+try:
+    path=sys.argv[1]
+except:
+    path = None
+bw_client = BwClient.resolve(path)
+window = MainPanel(bw_client=bw_client)
 
-    from bwUtilGui.mainPanel import MainPanel
-    # create the application and the main window
-    app = QtWidgets.QApplication(sys.argv)
-    try:
-        path=sys.argv[1]
-    except:
-        path = None
-    bw_client = BwClient.resolve(path)
-    window = MainPanel(bw_client=bw_client)
+# setup stylesheet
+apply_stylesheet(app, theme='dark_teal.xml')
 
-    # setup stylesheet
-    apply_stylesheet(app, theme='dark_teal.xml')
-
-    # run
-    window.show()
-    app.exec()
+# run
+window.show()
+app.exec()
