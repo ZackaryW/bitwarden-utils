877dc02b54e0174b1d48626509e9640d8f766668
ZackaryW <zackaryxcwang@gmail.com>
2022-07-17 17:46:22 -0700

    doc: caller

--- bwUtil/caller/__init__.py
+++ bwUtil/caller/__init__.py
@@ -6,16 +6,27 @@ from functools import cached_property as _cached_property
 import json as _json
 
 class BwClient(BwBaseClient):
+    """
+    bw cli wrapper
+    """
+
     def __init__(self, path: str) -> None:
         super().__init__(path)
         self.session = None
         self.lastSynced = None
 
     def _delCache(self, key):
+        """
+        resets cache_property
+        """
         if key in self.__dict__:
             del self.__dict__[key]
 
     def simpleRun(self, *args) -> BwResponse:
+        """
+        this method wraps around creating, running and closing a communication
+        """
+
         if self.session is not None:
             args = list(args) + ["--session", self.session]
 
@@ -24,6 +35,13 @@ class BwClient(BwBaseClient):
             return proc.commuicateObj()
 
     def runWithSession(self, *args):
+        """
+        this method calls simpleRun with session as suffix
+
+        Raises:
+            Exception: if session is not set
+        """
+
         if self.session is None:
             raise Exception("session not present")
         args = list(args) + ["--session", self.session]
@@ -34,13 +52,25 @@ class BwClient(BwBaseClient):
 
     @_cached_property
     def version(self):
+        """
+        returns the version of the bw cli
+        """
+
         return self.simpleRun("--version").rawLines[0]
 
     @_cached_property
     def isLoggedIn(self):
+        """
+        returns true if logged in
+        """
+
         return self.simpleRun("login").login_is_logged_in
 
     def login(self, username, password, totp= None):
+        """
+        logs in to bitwarden
+        """
+
         args = ["login", username, password]
         if totp is not None:
             args.extend(["--method","0","--code", totp])
@@ -57,16 +87,27 @@ class BwClient(BwBaseClient):
         return True
 
     def logout(self):
+        """
+        logs out of bitwarden
+        """
         with self.createCommunication("logout") as proc:
             self._delCache("isLoggedIn")
             return
 
     def unlock(self, password):
+        """
+        unlocks bitwarden
+        """
+
         res = self.simpleRun("unlock", password)
 
         self.session = res.session_extract
 
     def sync(self):
+        """
+        syncs bitwarden
+        """
+
         res = self.runWithSession("sync")
         if "Syncing complete." in res.raw:
             self.lastSynced = _datetime.now()
@@ -74,6 +115,16 @@ class BwClient(BwBaseClient):
         return False
 
     def get_attachment(self, item_id : str,item_name : str, attachment_name : str, output_path : str):
+        """
+        downloads an attachment from bitwarden
+
+        Args:
+            item_id (str)
+            item_name (str)
+            attachment_name (str)
+            output_path (str)
+        """
+
         if not _os.path.exists(_os.path.join(output_path, item_name)):
             _os.makedirs(_os.path.join(output_path, item_name), exist_ok=True)
         
@@ -94,6 +145,25 @@ class BwClient(BwBaseClient):
             pass
 
     def export_attachments(self, output_path :str, rawData : list = None):
+        """
+        exports all attachments from bitwarden to a given path
+
+        Args:
+            output_path (str)
+            rawData (list, optional): already exported data from other methods. used to cut down on calls to bitwarden. Defaults to None.
+
+        Yields:
+            i : the current count of progress
+
+        UseCase:
+            it can be used as a generator
+
+        ```py
+        for i in bw.export_attachments(output_path):
+            # output progress
+        ```
+        """
+
         if not rawData:
             if self.lastSynced is None and self.sync():
                 pass
@@ -122,6 +192,12 @@ class BwClient(BwBaseClient):
         
     @classmethod
     def find_nearby(cls):
+        """
+        attempts to find a bitwarden cli in cwd and environment variables
+
+        returns None if not found
+        """
+
         # check if BW_CLI_PATH is set
         if "BW_CLI_PATH" in _os.environ:
             return cls(_os.environ["BW_CLI_PATH"])
@@ -134,6 +210,7 @@ class BwClient(BwBaseClient):
 
     @classmethod
     def resolve(cls, path :str):
+
         if not path:
             return None
 
