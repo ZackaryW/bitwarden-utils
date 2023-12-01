e9debeacd8f15473cb12f6c3fddf53e0e46888c6
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2023-12-01 15:45:53 -0800

    ref: multi
    * make session a builtin var
    * path default valued to "bw"
    * fix: mtime for only_bw case
    * feat: added status and isLocked properties

---
+++ bitwarden_utils/_internal/status_model.py
@@ -0,0 +1,11 @@
+
+import typing
+from typing_extensions import TypedDict
+
+
+class Status(TypedDict):
+    serverUrl : typing.Optional[str]
+    lastSync : typing.Optional[str]
+    userEmail : typing.Optional[str]
+    userId : typing.Optional[str]
+    status : typing.Literal["locked", "unlocked"]
--- bitwarden_utils/core/proc.py
+++ bitwarden_utils/core/proc.py
@@ -1,35 +1,45 @@
 from dataclasses import dataclass
+import json
 import os
 import subprocess
+from typing import Any
 
-@dataclass(frozen=True)
+from bitwarden_utils._internal.status_model import Status
+
+@dataclass
 class BwProc:
-    path : str
+    path : str = "bw"
+    session : str = None
+
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        if __name == "path" and "path" in self.__dict__:
+            raise Exception("Cannot change path")
+        super().__setattr__(__name, __value)
 
     def __post_init__(self):
         if not os.path.exists(self.path) and not self.path == "bw":
             raise FileNotFoundError(f"{self.path} does not exist")
         
-        self.__recorded_mdate = os.path.getmtime(self.path)
+        if not self.only_bw:
+            self.__recorded_mdate = os.path.getmtime(self.path)
 
-    def __prep_args(self, *args, session = None):
+    def __prep_args(self, *args):
         cmd = [self.path]
         cmd += list(args)
-        if session:
-            cmd += ["--session", session]
+        if self.session:
+            cmd += ["--session", self.session]
 
         return cmd
 
     def exec(
         self, 
         *args, 
-        session = None, 
         strip : bool =True
     ):
-        if self.last_modified != self.__recorded_mdate:
+        if not self.only_bw and self.last_modified != self.__recorded_mdate:
             raise Exception("File has been tampered")
 
-        args = self.__prep_args(*args, session=session)
+        args = self.__prep_args(*args)
         ret = subprocess.run(args, stdout=subprocess.PIPE, check=True)
         # decode
         ret_output = ret.stdout.decode()
@@ -60,3 +70,20 @@ class BwProc:
     @property
     def version(self):
         return self.exec("--version")
+    
+    @property
+    def status(self)-> Status:
+        return json.loads(self.exec("status"))
+
+    @property
+    def isLocked(self):
+        return self.status["status"] == "locked"
+
+    @classmethod
+    def login(
+        cls,
+        username : str,
+        password : str,
+        totp : str = None
+    ):
+        pass
